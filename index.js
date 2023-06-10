18c017f7f3850a9cd86114345ec4f223fc98ffb0
Lucie Trojanová <trojanovalucie1@gmail.com>
2023-06-10 22:25:23 +0200

    přidání mapy.cz, apolenka

---
+++ index.js
@@ -0,0 +1,1 @@
+const zajimavost = document.querySelector('#zajimavost');
--- mapa.js
+++ mapa.js
@@ -2,3 +2,43 @@ var stred = SMap.Coords.fromWGS84(15, 50.08);
 var mapa = new SMap(JAK.gel('mapa'), stred, 7);
 mapa.addDefaultLayer(SMap.DEF_BASE).enable();
 mapa.addDefaultControls();
+
+var layer = new SMap.Layer.Marker();
+mapa.addLayer(layer);
+layer.enable();
+
+var card = new SMap.Card();
+card.getHeader().innerHTML = '<strong>Nautis</strong>';
+card.getBody().innerHTML = 'V Holešovičkách 593/1a, 182 00 Praha 8</em>';
+
+var options = {
+  title: 'Nautis',
+};
+var nautis = SMap.Coords.fromWGS84(14.4510939, 50.1175322);
+var marker = new SMap.Marker(nautis, 'Nautis', options);
+marker.decorate(SMap.Marker.Feature.Card, card);
+layer.addMarker(marker);
+
+card = new SMap.Card();
+card.getHeader().innerHTML = '<strong>RIC</strong>';
+card.getBody().innerHTML = 'náměstí Republiky 2686, 530 02 Pardubice</em>';
+
+var options = {
+  title: 'RIC',
+};
+var ric = SMap.Coords.fromWGS84(15.7770422, 50.0372325);
+marker = new SMap.Marker(ric, 'RIC', options);
+marker.decorate(SMap.Marker.Feature.Card, card);
+layer.addMarker(marker);
+
+card = new SMap.Card();
+card.getHeader().innerHTML = '<strong>Apolenka</strong>';
+card.getBody().innerHTML = 'Na Okrajích 156 Spojil, 530 02</em>';
+
+var options = {
+  title: 'Apolenka',
+};
+var apolenka = SMap.Coords.fromWGS84(15.8208144, 50.0480044);
+marker = new SMap.Marker(apolenka, 'Apolenka', options);
+marker.decorate(SMap.Marker.Feature.Card, card);
+layer.addMarker(marker);
