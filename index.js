ddbb290e93e7a43d1afe9bdd0d489de767bdbc65
Lucie Trojanová <trojanovalucie1@gmail.com>
2023-06-13 02:48:51 +0200

    organizace, texty

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Replace the normal space after a single letter word with a non-breaking space
+/* Replace the normal space after a single letter word with a non-breaking space
 function fixSingleLetterWords(text) {
   const prepositions = ['a', 'i', 'k', 'o', 's', 'u', 'v', 'z', 'A'];
   const regex = new RegExp(`\\b(${prepositions.join('|')})\\s`, 'gm');
@@ -6,9 +6,9 @@ function fixSingleLetterWords(text) {
   return fixedText;
 }
 
-// Apply the function to <p> elements
+Apply the function to <p> elements
 const paragraphs = document.getElementsByTagName('p');
 for (let i = 0; i < paragraphs.length; i++) {
   const paragraph = paragraphs[i];
   paragraph.innerHTML = fixSingleLetterWords(paragraph.innerHTML);
-}
+}*/
